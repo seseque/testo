0d718cbfcc53e2b0f9c3546b91fabfba20a5c931
seseque <vanchikovaseseg@gmail.com>
2020-06-23 12:12:38 +0700

    added JavaDoc

--- src/com/github/testo/annotations/After.java
+++ src/com/github/testo/annotations/After.java
@@ -5,6 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотацией @After помечаются те методы в классе,
+ * которые должны быть вызваны после методов с аннотацией @Test
+ * В случае если таких методов более одного в классе, выполняются все помеченные методы
+ * Порядок вызова не определен.
+ */
 @Target(value = ElementType.METHOD)
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface After {
